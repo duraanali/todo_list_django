a84e67f97ed177f79b5b75ac6946f61bfaa9f0f1
Duraan Ali <duraan522@gmail.com>
2020-06-16 04:30:14 -0500

    first commit

---
+++ todo/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Todo
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ['name', 'description', 'due_date']
+        
+    
